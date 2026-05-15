a6b02d287ccb8e76828b2df9d4cb317a20e7377f
manasdoesstuff <imorangeofficial@gmail.com>
2026-05-15 13:33:41 +0530

    Register service worker on load event

--- src/main.jsx
+++ src/main.jsx
@@ -12,3 +12,8 @@ if ('serviceWorker' in navigator) {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
 )
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(console.error);
+  });
+}
