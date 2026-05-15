16da366c4d12df8f08d4c972dfc21de145c458c9
manasdoesstuff <imorangeofficial@gmail.com>
2026-05-15 15:26:23 +0530

    Remove service worker registration from main.jsx
    Removed service worker registration code.

--- src/main.jsx
+++ src/main.jsx
@@ -12,8 +12,4 @@ if ('serviceWorker' in navigator) {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
 )
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(console.error);
-  });
-}
+
