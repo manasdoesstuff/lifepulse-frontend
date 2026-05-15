205fb94c8f60214b0a1baed42641cbd8d4104937
manasdoesstuff <imorangeofficial@gmail.com>
2026-05-15 09:52:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Register service worker for push notifications
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(console.error);
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode><App /></React.StrictMode>
 )
